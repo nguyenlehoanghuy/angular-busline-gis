327b7b3ec9608d02e4b67c315e294233983a6003
Huy Nguyen <nglehoanghuy066@gmail.com>
2024-06-20 18:04:22 +0700

    feat: home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LoginComponent,
+    component: HomeComponent,
     title: 'Bus line GIS',
   },
   {
