fc12db73bea015fb230a0ebdde6420763258de9d
Huy Nguyen <nglehoanghuy066@gmail.com>
2024-07-03 23:02:39 +0700

    feat: button addNewBusStation,  addNewBusLine

--- src/app/components/bus-line-datatable/bus-line-datatable.component.ts
+++ src/app/components/bus-line-datatable/bus-line-datatable.component.ts
@@ -1,36 +1,49 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BusLineService } from '../../services/busline.service';
 import { BusLine } from '../../interfaces/busline';
 
 @Component({
   selector: 'app-bus-line-datatable',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './bus-line-datatable.component.html',
-  styleUrl: './bus-line-datatable.component.css'
+  styleUrl: './bus-line-datatable.component.css',
 })
 export class BusLineDatatableComponent {
   busLineService = inject(BusLineService);
-  busLines: BusLine[] = [];
-  
+  busLines: any;
+
   constructor() {
     this.busLineService.getAllBusLines().subscribe({
       next: (res: any) => {
-        this.busLines = res;
+        this.busLines = res.data;
       },
       error: (err) => {
         console.log(err);
       },
-      complete: () => console.info('complete')
+      complete: () => console.info('complete'),
     });
   }
 
-  saveBusLine(id: number) {
+  addNewBusLine() {
+    this.busLines.push({
+      id: '',
+      name: '',
+      length: '',
+      price: '',
+      number_of_trips: '',
+      time_between_trips: '',
+      start_time_first: '',
+    });
+  }
+
+  saveBusLine(busLine: BusLine) {
     // do something
   }
 
-  removeBusLine(id: number) {
+  removeBusLine(busLine: BusLine) {
     // do something
   }
 }
--- src/app/components/bus-station-datatable/bus-station-datatable.component.ts
+++ src/app/components/bus-station-datatable/bus-station-datatable.component.ts
@@ -1,52 +1,66 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BusStationService } from '../../services/busstation.service';
 import { WardService } from '../../services/ward.service';
+import { BusStation } from '../../interfaces/busstation';
 
 @Component({
   selector: 'app-bus-station-datatable',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './bus-station-datatable.component.html',
-  styleUrl: './bus-station-datatable.component.css'
+  styleUrl: './bus-station-datatable.component.css',
 })
 export class BusStationDatatableComponent {
   busStationService = inject(BusStationService);
   wardService = inject(WardService);
   busStations: any;
   wards: any;
-  
+
   constructor() {
     this.busStationService.getAllBusStations().subscribe({
       next: (res: any) => {
-        this.busStations = res;
+        this.busStations = res.data;
       },
       error: (err) => {
         console.log(err);
       },
-      complete: () => console.info('complete')
+      complete: () => console.info('complete'),
     });
 
     this.wardService.getAllWards().subscribe({
       next: (res: any) => {
-        this.wards = res;
+        this.wards = res.data;
       },
       error: (err) => {
         console.log(err);
       },
-      complete: () => console.info('complete')
+      complete: () => console.info('complete'),
     });
   }
 
-  saveBusStation(id: number) {
-    // do something
+  addNewBusStation() {
+    this.busStations.push({
+      id: '',
+      name: '',
+      long: '',
+      lat: '',
+      address: '',
+      id_ward: '',
+    });
   }
 
-  removeBusStation(id: number) {
+  saveBusStation(busStation: BusStation) {
+    // do something
+    console.log(busStation);
+  }
+
+  removeBusStation(busStation: BusStation) {
     // do something
   }
 
   trackByWard(id_ward: string, id_district: string): string {
-    return id_ward + id_district;
+    return id_district + id_ward;
   }
 }
