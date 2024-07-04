880966e61d3bcc287717593b80169f9971d96761
Huy Nguyen <nglehoanghuy066@gmail.com>
2024-07-04 09:29:35 +0700

    feat: schedule of bus line in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BusLineDatatableComponent } from '../components/bus-line-datatable/bus-line-datatable.component';
 import { BusStationDatatableComponent } from '../components/bus-station-datatable/bus-station-datatable.component';
+import { StationLineDatatableComponent } from '../components/station-line-datatable/station-line-datatable.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [BusLineDatatableComponent, BusStationDatatableComponent],
+  imports: [BusLineDatatableComponent, BusStationDatatableComponent, StationLineDatatableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
